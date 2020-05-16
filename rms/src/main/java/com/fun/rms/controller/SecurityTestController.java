9bcbf9e4e0a46f12ac824b96595628b35c74b606
jussi <45388631+jussihailu1@users.noreply.github.com>
2020-05-16 12:59:15 +0200

    .

--- rms/src/main/java/com/fun/rms/controller/SecurityTestController.java
+++ rms/src/main/java/com/fun/rms/controller/SecurityTestController.java
@@ -2,10 +2,6 @@ package com.fun.rms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +11,6 @@ import com.fun.rms.dto.ResponseDTO;
 import com.fun.rms.enums.Response;
 import com.fun.rms.security.models.AuthenticationRequest;
 import com.fun.rms.security.models.AuthenticationResponse;
-import com.fun.rms.security.util.JwtUtil;
 import com.fun.rms.service.AuthenticationService;
 import com.fun.rms.service.UserService;
 
@@ -23,27 +18,20 @@ import com.fun.rms.service.UserService;
 public class SecurityTestController {
 
 	@Autowired
-	private AuthenticationManager authenticationManager;
-
-	@Autowired
 	private AuthenticationService authenticationService;
-
+	
 	@Autowired
-	private JwtUtil jwtTokenUtil;
+	private UserService userService;
 
 	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
 	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
 			throws Exception {
-		if (authenticationService.loginCodeExists(authenticationRequest.getLoginCode())) {
-			UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-					authenticationRequest.getLoginCode(), authenticationRequest.getLoginCode());
-			authenticationManager.authenticate(authToken);
-			final UserDetails userDetails = authenticationService.loadUserByUsername(authenticationRequest.getLoginCode());
-			final String jwt = jwtTokenUtil.generateToken(userDetails);
-
+		if (userService.loginCodeExists(authenticationRequest.getLoginCode())) {
+			String jwt = authenticationService.authenticate(authenticationRequest.getLoginCode());
 			return ResponseEntity.ok(new AuthenticationResponse(jwt));
 		} else {
 			return ResponseEntity.ok(ResponseDTO.send(Response.WRONG_CREDENTIALS));
 		}
 	}
+	
 }
--- rms/src/main/java/com/fun/rms/service/AuthenticationService.java
+++ rms/src/main/java/com/fun/rms/service/AuthenticationService.java
@@ -5,25 +5,40 @@ import java.util.ArrayList;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.fun.rms.security.util.JwtUtil;
+
 @Service
 @Transactional
 public class AuthenticationService implements UserDetailsService {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
 	@Autowired
 	private UserService userService;
+
+	@Autowired
+	private JwtUtil jwtTokenUtil;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		String loginCode = userService.findByLoginCode(username).getLoginCode();
 		return new org.springframework.security.core.userdetails.User(loginCode, loginCode, new ArrayList<>());
 	}
-	
-	public Boolean loginCodeExists(String loginCode) {
-		return userService.loginCodeExists(loginCode);
+
+	public String authenticate(String loginCode) {
+		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(loginCode, loginCode);
+		authenticationManager.authenticate(authToken);
+		final UserDetails userDetails = loadUserByUsername(loginCode);
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+		return jwt;
 	}
-	
+
 }

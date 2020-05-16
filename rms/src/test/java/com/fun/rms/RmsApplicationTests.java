859c180878d014845424745d601f319b3841ce3e
jussi <45388631+jussihailu1@users.noreply.github.com>
2020-05-16 22:07:27 +0200

    .

--- rms/src/test/java/com/fun/rms/RmsApplicationTests.java
+++ rms/src/test/java/com/fun/rms/RmsApplicationTests.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(MockitoJUnitRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 class RmsApplicationTests {
 
